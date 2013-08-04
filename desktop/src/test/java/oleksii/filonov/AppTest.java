91a81423475207bcf5577d9d92ab24a41cbbce8d
Oleksii Filonov <asfilonov@gmail.com>
2013-08-04 17:33:04 +0300

    Add desktop module

---
+++ desktop/src/test/java/oleksii/filonov/AppTest.java
@@ -0,0 +1,16 @@
+package oleksii.filonov;
+
+import oleksii.filonov.writer.XSSFBuilder;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+	@Test
+	public void testName() {
+		final XSSFBuilder xssfBuilder = new XSSFBuilder();
+	}
+}
