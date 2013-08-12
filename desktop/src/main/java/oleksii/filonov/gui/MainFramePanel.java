dc964df4c791fc24b85b3567611774d8d500cc82
Oleksii Filonov <asfilonov@gmail.com>
2013-08-12 22:10:02 +0300

    Remove + buttom at the bottom of the panel

--- desktop/src/main/java/oleksii/filonov/gui/MainFramePanel.java
+++ desktop/src/main/java/oleksii/filonov/gui/MainFramePanel.java
@@ -2,7 +2,6 @@ package oleksii.filonov.gui;
 
 import java.awt.BorderLayout;
 
-import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.border.EmptyBorder;
@@ -11,18 +10,17 @@ import oleksii.filonov.model.MainTableModel;
 
 public class MainFramePanel extends JPanel {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    public MainFramePanel() {
-        setBorder(new EmptyBorder(5, 5, 5, 5));
-        setLayout(new BorderLayout(0, 0));
-        final TopButtonsPanel topButtonsPanel = new TopButtonsPanel();
-        add(topButtonsPanel, BorderLayout.NORTH);
-        final ComponentsLocator locator = ComponentsLocator.getInstanse();
-        locator.setTable(new MainTable(new MainTableModel()));
-        final JScrollPane scrollPane = new JScrollPane(locator.getTable());
-        add(scrollPane, BorderLayout.CENTER);
-        add(new JButton("+"), BorderLayout.SOUTH);
-    }
+	public MainFramePanel() {
+		setBorder(new EmptyBorder(5, 5, 5, 5));
+		setLayout(new BorderLayout(0, 0));
+		final TopButtonsPanel topButtonsPanel = new TopButtonsPanel();
+		add(topButtonsPanel, BorderLayout.NORTH);
+		final ComponentsLocator locator = ComponentsLocator.getInstanse();
+		locator.setTable(new MainTable(new MainTableModel()));
+		final JScrollPane scrollPane = new JScrollPane(locator.getTable());
+		add(scrollPane, BorderLayout.CENTER);
+	}
 
 }
