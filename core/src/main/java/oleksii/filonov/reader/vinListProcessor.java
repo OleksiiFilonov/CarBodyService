e8e52624a01004707a1a1f48701bf5aabe99ff0b
Oleksii_Filonov <Oleksii.Filonov@travisperkins.co.uk>
2013-09-23 03:51:04 +0400

    add dummy test for vin list - description map

---
+++ core/src/main/java/oleksii/filonov/reader/vinListProcessor.java
@@ -0,0 +1,13 @@
+package oleksii.filonov.reader;
+
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+public class VinListProcessor {
+
+    public Map<String, String> linkVinListIdWithDescription(File campaignFile) {
+        return new HashMap<>();
+    }
+}
---
+++ core/src/test/java/oleksii/filonov/reader/vinListProcessorTest.java
@@ -0,0 +1,21 @@
+package oleksii.filonov.reader;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+import java.util.Map;
+
+import oleksii.filonov.TestConstants;
+import org.junit.Test;
+
+public class VinListProcessorTest {
+
+    private VinListProcessor vinListProcessor = new VinListProcessor();
+
+
+    @Test
+    public void formVinListIdDescriptionMap() {
+        Map<String, String> vinListDescriptionMap = vinListProcessor.linkVinListIdWithDescription(TestConstants.CAMPAIGN_FILE);
+        assertThat(vinListDescriptionMap.size(), equalTo(38));
+    }
+}
