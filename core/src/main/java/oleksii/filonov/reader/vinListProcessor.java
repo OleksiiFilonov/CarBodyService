a1b4d88efbc92145e52d1bf1c1cf92fa0c455f21
Oleksii_Filonov <Oleksii.Filonov@travisperkins.co.uk>
2013-09-24 23:33:34 +0400

    Implement functionality for vin list id - description map

--- core/src/main/java/oleksii/filonov/reader/vinListProcessor.java
+++ core/src/main/java/oleksii/filonov/reader/vinListProcessor.java
@@ -4,22 +4,40 @@ package oleksii.filonov.reader;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.ss.usermodel.*;
 
 public class VinListProcessor {
 
-    public Map<String, String> mapVinListIdToDescription(File campaignFile) {
+    private ColumnReaderHelper columnReaderHelper;
+
+    public Map<String, String> mapVinListIdToDescription(final File campaignFile, final String vinColumnTitle, final String descriptionTitle) {
+        final Map<String, String> result = new HashMap<>();
         try {
             final Workbook campaignWB = WorkbookFactory.create(campaignFile);
             final Sheet vinListSheet = campaignWB.getSheetAt(0);
+            final Iterator<Row> vinListIterator = vinListSheet.rowIterator();
+            final Cell vinTitleCell = columnReaderHelper.findCell(vinListIterator, vinColumnTitle);
+            final int vinColumnIndex = vinTitleCell.getColumnIndex();
+            final int descriptionColumnIndex = columnReaderHelper.findCellFrom(vinTitleCell, vinListIterator, descriptionTitle).getColumnIndex();
+            while (vinListIterator.hasNext()) {
+                final Row vinListRow = vinListIterator.next();
+                final Cell vinListCell = vinListRow.getCell(vinColumnIndex);
+                Cell descriptionCell = vinListRow.getCell(descriptionColumnIndex);
+                if(vinListCell != null && descriptionCell != null) {
+                    result.put(vinListCell.getStringCellValue().trim(), descriptionCell.getStringCellValue().trim());
+                }
+            }
         } catch (IOException  | InvalidFormatException e) {
             throw new ReadDataException("Exception occurred while opening campaign file", e);
         }
-        return new HashMap<>();
+        return result;
+    }
+
+    public void setColumnReaderHelper(final ColumnReaderHelper columnReaderHelper) {
+        this.columnReaderHelper = columnReaderHelper;
     }
 }
--- core/src/test/java/oleksii/filonov/reader/vinListProcessorTest.java
+++ core/src/test/java/oleksii/filonov/reader/vinListProcessorTest.java
@@ -1,21 +1,30 @@
 package oleksii.filonov.reader;
 
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 import java.util.Map;
 
 import oleksii.filonov.TestConstants;
+import org.junit.Before;
 import org.junit.Test;
 
 public class VinListProcessorTest {
 
-    private VinListProcessor vinListProcessor = new VinListProcessor();
+    private VinListProcessor vinListProcessor;
+
+    @Before
+    public void setUp() {
+        vinListProcessor = new VinListProcessor();
+        vinListProcessor.setColumnReaderHelper(new ColumnReaderHelper());
+    }
 
 
     @Test
     public void formVinListIdDescriptionMap() {
-        Map<String, String> vinListDescriptionMap = vinListProcessor.mapVinListIdToDescription(TestConstants.CAMPAIGN_FILE);
+        Map<String, String> vinListDescriptionMap = vinListProcessor.mapVinListIdToDescription(TestConstants.CAMPAIGN_FILE, "Номер кампании", "Описание");
         assertThat(vinListDescriptionMap.size(), equalTo(38));
+        assertEquals("ЗАМЕНА ПОЛОЖИТЕЛЬНОЙ КЛЕММЫ АККУМУЛЯТОРНОЙ БАТАРЕИ(TSB No. In English : HCE11-92-P550-YFHG)", vinListDescriptionMap.get("10C059"));
     }
 }
