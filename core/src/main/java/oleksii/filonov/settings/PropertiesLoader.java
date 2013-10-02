ba07a4b47250a3033c9b2bb59b1955fec48dca03
Oleksii Filonov <asfilonov@gmail.com>
2013-10-02 22:36:02 +0300

    Read settings file from jar

--- core/src/main/java/oleksii/filonov/settings/PropertiesLoader.java
+++ core/src/main/java/oleksii/filonov/settings/PropertiesLoader.java
@@ -1,26 +1,25 @@
 package oleksii.filonov.settings;
 
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.Properties;
 
 public class PropertiesLoader {
 
-    private Settings settings;
+	private Settings settings;
 
-    public Properties loadProperties() throws IOException {
-        Properties prop = new Properties();
-        prop.load(new FileReader(Paths.get("", "src", "main", "resources", "config", "settings.properties").toFile()));
-        settings = new Settings();
-        settings.setCampaignColumnNumberCampaignTitle(prop.getProperty("campaign.column.title.numberCampaign"));
-        settings.setCampaignColumnDescriptionTitle(prop.getProperty("campaign.column.title.description"));
-        settings.setCampaignColumnVinListIdTitle(prop.getProperty("campaign.column.title.vinListId"));
-        settings.setClientColumnBodyNumber(prop.getProperty("client.column.title.bodyNumber"));
-        return prop;
-    }
+	public Properties loadProperties() throws IOException {
+		final Properties prop = new Properties();
+		prop.load(getClass().getResourceAsStream(Paths.get("/", "config", "settings.properties").toString()));
+		settings = new Settings();
+		settings.setCampaignColumnNumberCampaignTitle(prop.getProperty("campaign.column.title.numberCampaign"));
+		settings.setCampaignColumnDescriptionTitle(prop.getProperty("campaign.column.title.description"));
+		settings.setCampaignColumnVinListIdTitle(prop.getProperty("campaign.column.title.vinListId"));
+		settings.setClientColumnBodyNumber(prop.getProperty("client.column.title.bodyNumber"));
+		return prop;
+	}
 
-    public Settings getSettings() {
-        return settings;
-    }
+	public Settings getSettings() {
+		return settings;
+	}
 }
