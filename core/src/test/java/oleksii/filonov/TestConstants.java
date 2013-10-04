c408389373b4347bbac799e3f569eed71120775f
Oleksii Filonov <asfilonov@gmail.com>
2013-10-04 22:56:00 +0300

    Pass to Facade Integration test new files

--- core/src/test/java/oleksii/filonov/TestConstants.java
+++ core/src/test/java/oleksii/filonov/TestConstants.java
@@ -5,8 +5,11 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class TestConstants {
-	public static final Path TARGET_RESOURCE = Paths.get("", "target");
+	private static final Path TARGET_RESOURCE = Paths.get("", "target");
+	private static final Path RESOURCE_PATH = Paths.get("", "src", "test", "resources");
 	public static final Path LINKED_RESULT_PATH = TARGET_RESOURCE.resolve("resultLinks.xls");
-	public static final File CAMPAIGN_FILE = Paths.get("", "src", "test", "resources", "Campaign.xlsx").toFile();
-	public static final File CLIENT_FILE = Paths.get("", "src", "test", "resources", "Clients.xls").toFile();
+	public static final File CAMPAIGN_FILE = RESOURCE_PATH.resolve("Campaign.xlsx").toFile();
+	public static final File CLIENT_FILE = RESOURCE_PATH.resolve("Clients.xls").toFile();
+	public static final File CAMPAIGN_FILE2 = RESOURCE_PATH.resolve("Campaign2.xlsx").toFile();
+	public static final File CLIENT_FILE2 = RESOURCE_PATH.resolve("Clients2.xlsx").toFile();
 }
