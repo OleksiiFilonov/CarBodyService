e125d4276980f21c7d6a6ac9577d621b5e08b1dd
Oleksii Filonov <asfilonov@gmail.com>
2013-10-04 20:01:36 +0300

    Replace default load properties test for load from specific place test

--- core/src/test/java/oleksii/filonov/settings/PropertiesLoaderTest.java
+++ core/src/test/java/oleksii/filonov/settings/PropertiesLoaderTest.java
@@ -3,20 +3,32 @@ package oleksii.filonov.settings;
 import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
+import java.nio.file.Paths;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class PropertiesLoaderTest {
 
-    private PropertiesLoader propertiesLoader = new PropertiesLoader();
+	private final PropertiesLoader propertiesLoader = new PropertiesLoader();
 
-    @Test
-    public void loadProperties() throws IOException {
-        propertiesLoader.loadDefaultProperties();
-        Settings settings = propertiesLoader.getSettings();
-        assertEquals("Номер кампании", settings.getCampaignColumnNumberCampaignTitle());
-        assertEquals("Описание", settings.getCampaignColumnDescriptionTitle());
-        assertEquals("VIN", settings.getCampaignColumnVinListIdTitle());
-        assertEquals("Номер кузова", settings.getClientColumnBodyNumber());
-    }
+	@Ignore
+	@Test
+	public void loadDefaultProperties() throws IOException {
+		propertiesLoader.loadDefaultProperties();
+		final Settings settings = propertiesLoader.getSettings();
+		assertEquals("Номер кампании", settings.getCampaignColumnNumberCampaignTitle());
+		assertEquals("Описание", settings.getCampaignColumnDescriptionTitle());
+		assertEquals("VIN", settings.getCampaignColumnVinListIdTitle());
+		assertEquals("Номер кузова", settings.getClientColumnBodyNumber());
+	}
+
+	public void loadPropertiesFrom() throws IOException {
+		propertiesLoader.loadPropertiesFrom(Paths.get("", "resources", "config", "path_to_settings.properties"));
+		final Settings settings = propertiesLoader.getSettings();
+		assertEquals("Номер кампании", settings.getCampaignColumnNumberCampaignTitle());
+		assertEquals("Описание", settings.getCampaignColumnDescriptionTitle());
+		assertEquals("VIN", settings.getCampaignColumnVinListIdTitle());
+		assertEquals("Номер кузова", settings.getClientColumnBodyNumber());
+	}
 }
