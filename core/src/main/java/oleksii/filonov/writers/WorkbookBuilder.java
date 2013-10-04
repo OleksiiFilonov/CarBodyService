39b662911765f6d98c79f07f6cbcfcf30eedbfc6
Oleksii Filonov <asfilonov@gmail.com>
2013-10-04 23:34:31 +0300

    Add comprehensive test for 2 variant of client and campaign file

--- core/src/main/java/oleksii/filonov/writers/WorkbookBuilder.java
+++ core/src/main/java/oleksii/filonov/writers/WorkbookBuilder.java
@@ -1,59 +1,70 @@
 package oleksii.filonov.writers;
 
-import java.io.*;
-import java.util.*;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
-import com.google.common.collect.*;
-import org.apache.poi.openxml4j.exceptions.*;
-import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.CreationHelper;
+import org.apache.poi.ss.usermodel.Hyperlink;
+import org.apache.poi.ss.usermodel.IndexedColors;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+
+import com.google.common.collect.ListMultimap;
 
 public class WorkbookBuilder implements DataBuilder {
 
-    private static final int SHIFT_ROW_OFFSET = 1;
-    private static final int VIN_LINK_OFFSET = 1;
-    private static final int VIN_DESCRIPTION_OFFSET = 2;
-    private static final char[] COLUMN_INDEXES = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
-            'P', 'Q', 'R', 'S', 'T', 'Y', 'V', 'W', 'X', 'Y', 'Z' };
-    private static final int EXCEL_ROW_OFFSET = 1;
-    private static final int VIN_LIST_LINK_COL_INDEX = 6;
-    private static final int VIN_DESC_COL_INDEX = 7;
+	private static final int SHIFT_ROW_OFFSET = 1;
+	private static final int VIN_LINK_OFFSET = 1;
+	private static final int VIN_DESCRIPTION_OFFSET = 2;
+	private static final char[] COLUMN_INDEXES = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+			'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'Y', 'V', 'W', 'X', 'Y', 'Z' };
+	private static final int EXCEL_ROW_OFFSET = 1;
+	private static final int VIN_LIST_LINK_COL_INDEX = 6;
+	private static final int VIN_DESC_COL_INDEX = 7;
 
-    private Workbook clientWorkbook;
+	private Workbook clientWorkbook;
 	private CellStyle foundCellStyle;
-    private CellStyle linkCellStyle;
+	private CellStyle linkCellStyle;
 	private CreationHelper creationHelper;
-    private String pathToCampaignFile;
-    private Map<String, String> vinListDescriptionMap;
+	private String pathToCampaignFile;
+	private Map<String, String> vinListDescriptionMap;
 
-    @Override
-	public void useWorkbook(Workbook clientWorkbook) throws IOException, InvalidFormatException {
+	@Override
+	public void useWorkbook(final Workbook clientWorkbook) throws IOException, InvalidFormatException {
 		this.clientWorkbook = clientWorkbook;
 		creationHelper = this.clientWorkbook.getCreationHelper();
 		initFoundCellStyle();
-        initLinkCellStyle();
+		initLinkCellStyle();
 	}
 
 	@Override
 	public void saveToFile(final File fileToSave) throws IOException {
-        clientWorkbook.getSheetAt(0).autoSizeColumn(VIN_LIST_LINK_COL_INDEX);
-        clientWorkbook.getSheetAt(0).autoSizeColumn(VIN_DESC_COL_INDEX);
+		clientWorkbook.getSheetAt(0).autoSizeColumn(VIN_LIST_LINK_COL_INDEX);
+		clientWorkbook.getSheetAt(0).autoSizeColumn(VIN_DESC_COL_INDEX);
 		final FileOutputStream recordStream = new FileOutputStream(fileToSave);
 		clientWorkbook.write(recordStream);
 		recordStream.close();
 	}
 
-    @Override
-    public void setPathToCampaignFile(String pathToCampaignFile) {
-        this.pathToCampaignFile = pathToCampaignFile;
-    }
+	@Override
+	public void setPathToCampaignFile(final String pathToCampaignFile) {
+		this.pathToCampaignFile = pathToCampaignFile;
+	}
 
-    @Override
+	@Override
 	public void assignTasks(final Cell[] bodyIdCells, final ListMultimap<String, Cell> linksToBodies) {
 		for (final Cell bodyIdCell : bodyIdCells) {
 			final List<Cell> vinLinks = linksToBodies.get(bodyIdCell.getStringCellValue());
 			if (!vinLinks.isEmpty()) {
-                bodyIdCell.setCellStyle(foundCellStyle);
-                final Row bodyIdRow = bodyIdCell.getRow();
+				bodyIdCell.setCellStyle(foundCellStyle);
+				final Row bodyIdRow = bodyIdCell.getRow();
 				linkBodyIdsWithVINLists(bodyIdCell.getColumnIndex(), vinLinks, bodyIdRow);
 				int cellIndex = 1;
 				while (bodyIdRow.getCell(cellIndex) != null) {
@@ -63,52 +74,57 @@ public class WorkbookBuilder implements DataBuilder {
 		}
 	}
 
-	private void linkBodyIdsWithVINLists(final int bodyIdColumnIndex, final List<Cell> foundBodyIdsOnVinLists, final Row bodyIdRow) {
+	private void linkBodyIdsWithVINLists(final int bodyIdColumnIndex, final List<Cell> foundBodyIdsOnVinLists,
+			final Row bodyIdRow) {
 
-        createLinkToVinListCell(bodyIdColumnIndex, foundBodyIdsOnVinLists.get(0), bodyIdRow);
+		createLinkToVinListCell(bodyIdColumnIndex, foundBodyIdsOnVinLists.get(0), bodyIdRow);
 
-        final Sheet clientSheet = bodyIdRow.getSheet();
-        final int bodyIdRowRowNum = bodyIdRow.getRowNum();
-        for (int i = 1; i < foundBodyIdsOnVinLists.size(); i++) {
-            clientSheet.shiftRows(bodyIdRowRowNum + i, clientSheet.getLastRowNum(), SHIFT_ROW_OFFSET);
-            final Row newRow = clientSheet.createRow(bodyIdRowRowNum + i);
-            final Cell linkVinCell = foundBodyIdsOnVinLists.get(i);
-            createLinkToVinListCell(bodyIdColumnIndex, linkVinCell, newRow);
-        }
-    }
-
-    private void createLinkToVinListCell(final int bodyIdColumnIndex, final Cell foundBodyIdCellOnVinList, final Row bodyIdRow) {
-        final Cell linkToVin = bodyIdRow.createCell(bodyIdColumnIndex + VIN_LINK_OFFSET, Cell.CELL_TYPE_STRING);
-        String sheetName = foundBodyIdCellOnVinList.getSheet().getSheetName();
-        linkToVin.setCellValue(sheetName);
-        final Hyperlink cellHyperlink = creationHelper.createHyperlink(Hyperlink.LINK_FILE);
-        cellHyperlink.setAddress(pathToCampaignFile + '#' + linkToCell(foundBodyIdCellOnVinList));
-        cellHyperlink.setLabel(sheetName);
-        linkToVin.setHyperlink(cellHyperlink);
-        linkToVin.setCellStyle(linkCellStyle);
-        final Cell vinDescriptionCell = bodyIdRow.createCell(bodyIdColumnIndex + VIN_DESCRIPTION_OFFSET, Cell.CELL_TYPE_STRING);
-        vinDescriptionCell.setCellValue(vinListDescriptionMap.get(sheetName));
-        vinDescriptionCell.setCellStyle(linkCellStyle);
-    }
-
-    private void initLinkCellStyle() {
-		linkCellStyle = clientWorkbook.createCellStyle();
-        linkCellStyle.setFillForegroundColor(IndexedColors.YELLOW.getIndex());
-        linkCellStyle.setFillPattern(CellStyle.SOLID_FOREGROUND);
+		final Sheet clientSheet = bodyIdRow.getSheet();
+		final int bodyIdRowRowNum = bodyIdRow.getRowNum();
+		for (int i = 1; i < foundBodyIdsOnVinLists.size(); i++) {
+			clientSheet.shiftRows(bodyIdRowRowNum + i, clientSheet.getLastRowNum(), SHIFT_ROW_OFFSET);
+			final Row newRow = clientSheet.createRow(bodyIdRowRowNum + i);
+			final Cell linkVinCell = foundBodyIdsOnVinLists.get(i);
+			createLinkToVinListCell(bodyIdColumnIndex, linkVinCell, newRow);
+		}
 	}
 
-    private void initFoundCellStyle() {
+	private void createLinkToVinListCell(final int bodyIdColumnIndex, final Cell foundBodyIdCellOnVinList,
+			final Row bodyIdRow) {
+		final Cell linkToVin = bodyIdRow.createCell(bodyIdColumnIndex + VIN_LINK_OFFSET, Cell.CELL_TYPE_STRING);
+		final String sheetName = foundBodyIdCellOnVinList.getSheet().getSheetName();
+		linkToVin.setCellValue(sheetName);
+		final Hyperlink cellHyperlink = creationHelper.createHyperlink(Hyperlink.LINK_DOCUMENT);
+		cellHyperlink.setAddress(pathToCampaignFile + '#' + linkToCell(foundBodyIdCellOnVinList));
+		cellHyperlink.setLabel(sheetName);
+		linkToVin.setHyperlink(cellHyperlink);
+		linkToVin.setCellStyle(linkCellStyle);
+		final Cell vinDescriptionCell = bodyIdRow.createCell(bodyIdColumnIndex + VIN_DESCRIPTION_OFFSET,
+				Cell.CELL_TYPE_STRING);
+		vinDescriptionCell.setCellValue(vinListDescriptionMap.get(sheetName));
+		vinDescriptionCell.setCellStyle(linkCellStyle);
+	}
+
+	private void initLinkCellStyle() {
+		linkCellStyle = clientWorkbook.createCellStyle();
+		linkCellStyle.setFillForegroundColor(IndexedColors.YELLOW.getIndex());
+		linkCellStyle.setFillPattern(CellStyle.SOLID_FOREGROUND);
+	}
+
+	private void initFoundCellStyle() {
 		foundCellStyle = clientWorkbook.createCellStyle();
-        foundCellStyle.setFillForegroundColor(IndexedColors.RED.getIndex());
+		foundCellStyle.setFillForegroundColor(IndexedColors.RED.getIndex());
 		foundCellStyle.setFillPattern(CellStyle.SOLID_FOREGROUND);
 	}
 
-    private String linkToCell(final Cell bodyIdCellOnVinList) {
-        return "'" + bodyIdCellOnVinList.getSheet().getSheetName() + "'!" + COLUMN_INDEXES[bodyIdCellOnVinList.getColumnIndex()]
-                + (bodyIdCellOnVinList.getRowIndex() + EXCEL_ROW_OFFSET);
-    }
-    @Override
-    public void setVinListDescriptionMap(final Map<String, String> vinListDescriptionMap) {
-        this.vinListDescriptionMap = vinListDescriptionMap;
-    }
+	private String linkToCell(final Cell bodyIdCellOnVinList) {
+		return "'" + bodyIdCellOnVinList.getSheet().getSheetName() + "'!"
+				+ COLUMN_INDEXES[bodyIdCellOnVinList.getColumnIndex()]
+				+ (bodyIdCellOnVinList.getRowIndex() + EXCEL_ROW_OFFSET);
+	}
+
+	@Override
+	public void setVinListDescriptionMap(final Map<String, String> vinListDescriptionMap) {
+		this.vinListDescriptionMap = vinListDescriptionMap;
+	}
 }
--- core/src/test/java/oleksii/filonov/processors/WorkbookProcessorFacadeIntegrationTest.java
+++ core/src/test/java/oleksii/filonov/processors/WorkbookProcessorFacadeIntegrationTest.java
@@ -3,24 +3,50 @@ package oleksii.filonov.processors;
 import static oleksii.filonov.TestConstants.CAMPAIGN_FILE2;
 import static oleksii.filonov.TestConstants.CLIENT_FILE2;
 import static oleksii.filonov.TestConstants.LINKED_RESULT_PATH2;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
 
 import java.io.IOException;
+import java.util.Iterator;
 
+import oleksii.filonov.readers.ColumnReaderHelper;
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.junit.Before;
 import org.junit.Test;
 
 public class WorkbookProcessorFacadeIntegrationTest {
 
+	private static final String DESC_10C116 = "ЗАМЕНА КЛЕММ КАБЕЛЯ АКБ(TSB No. In English : HCE11-11-P180-RBMDENFDLMTQCMHRNF)";
+
 	private DataProcessorFacade dataProcessorFacade;
+
+	private ColumnReaderHelper columnReaderHelper;
 
 	@Before
 	public void setUp() throws IOException {
 		final WorkbookProcessorFacade workbookProcessorFacade = new WorkbookProcessorFacade();
 		dataProcessorFacade = workbookProcessorFacade;
+		columnReaderHelper = new ColumnReaderHelper();
 	}
 
 	@Test
-	public void createResultFile() {
+	public void createResultFile() throws InvalidFormatException, IOException {
 		dataProcessorFacade.createResultFile(CLIENT_FILE2, CAMPAIGN_FILE2, LINKED_RESULT_PATH2.toFile());
+
+		final Workbook workbookForVerification = WorkbookFactory.create(LINKED_RESULT_PATH2.toFile());
+		final Sheet verifyClientSheet = workbookForVerification.getSheetAt(0);
+		final Iterator<Row> clientIterator = verifyClientSheet.rowIterator();
+		final Cell cell_KMHSH81XDBU763142 = columnReaderHelper.findCell(clientIterator, "KMHSH81XDBU763142");
+		final Cell cell_KMHSH81XDBU763142_vinLIst = columnReaderHelper.findCellFrom(cell_KMHSH81XDBU763142,
+				clientIterator, "10C116");
+		final Cell cell_KMHSH81XDBU763142_description = columnReaderHelper.findCellFrom(cell_KMHSH81XDBU763142_vinLIst,
+				clientIterator, DESC_10C116);
+		assertThat(cell_KMHSH81XDBU763142_description.getRowIndex(), equalTo(cell_KMHSH81XDBU763142.getRowIndex()));
 	}
 }
