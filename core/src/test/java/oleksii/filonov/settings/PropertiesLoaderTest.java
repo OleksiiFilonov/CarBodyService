95e98e651e39f0f43d25f02869c25a614e1f7218
Oleksii Filonov <asfilonov@gmail.com>
2013-10-04 22:44:07 +0300

    Create test for default and explicetly set PropertyLoader

--- core/src/test/java/oleksii/filonov/settings/PropertiesLoaderTest.java
+++ core/src/test/java/oleksii/filonov/settings/PropertiesLoaderTest.java
@@ -5,14 +5,12 @@ import static org.junit.Assert.assertEquals;
 import java.io.IOException;
 import java.nio.file.Paths;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class PropertiesLoaderTest {
 
 	private final PropertiesLoader propertiesLoader = new PropertiesLoader();
 
-	@Ignore
 	@Test
 	public void loadDefaultProperties() throws IOException {
 		propertiesLoader.loadDefaultProperties();
@@ -20,11 +18,13 @@ public class PropertiesLoaderTest {
 		assertEquals("Номер кампании", settings.getCampaignColumnNumberCampaignTitle());
 		assertEquals("Описание", settings.getCampaignColumnDescriptionTitle());
 		assertEquals("VIN", settings.getCampaignColumnVinListIdTitle());
-		assertEquals("Номер кузова", settings.getClientColumnBodyNumber());
+		assertEquals("VIN номер автомобиля", settings.getClientColumnBodyNumber());
 	}
 
+	@Test
 	public void loadPropertiesFrom() throws IOException {
-		propertiesLoader.loadPropertiesFrom(Paths.get("", "resources", "config", "path_to_settings.properties"));
+		propertiesLoader.loadPropertiesFrom(Paths.get("", "src", "test", "resources", "config",
+				"path_to_settings.properties"));
 		final Settings settings = propertiesLoader.getSettings();
 		assertEquals("Номер кампании", settings.getCampaignColumnNumberCampaignTitle());
 		assertEquals("Описание", settings.getCampaignColumnDescriptionTitle());
