bc6ebf525ddb847f6fcfd54dd21d44b948cc2190
Oleksii Filonov <Oleksii.Filonov@travisperkins.co.uk>
2013-10-13 12:57:09 +0300

    Add failing integration test for description offset

--- core/src/test/java/oleksii/filonov/writers/WorkbookBuilderIntegrationTest.java
+++ core/src/test/java/oleksii/filonov/writers/WorkbookBuilderIntegrationTest.java
@@ -1,8 +1,7 @@
 package oleksii.filonov.writers;
 
-import static oleksii.filonov.TestConstants.CAMPAIGN_FILE;
-import static oleksii.filonov.TestConstants.CLIENT_FILE;
-import static oleksii.filonov.TestConstants.LINKED_RESULT_PATH;
+import static oleksii.filonov.TestConstants.*;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 import java.io.IOException;
@@ -31,9 +30,9 @@ import com.google.common.collect.ListMultimap;
 
 public class WorkbookBuilderIntegrationTest {
 
-	private static final String BODY_ID_MARKER = "Номер кузова";
+    private static final String BODY_ID_MARKER = "Номер кузова";
 	private static final String VIN_MARKER = "VIN";
-	private static final int VIN_LINK_COLUMN_INDEX = 6;
+    private static final int VIN_LINK_COLUMN_INDEX = 6;
 	private static final int VIN_DESC_COLUMN_INDEX = 7;
 	private static final Matcher<Integer> LINK_COL_INDEX = CoreMatchers.equalTo(VIN_LINK_COLUMN_INDEX);
 	private static final Matcher<Integer> DESC_COL_INDEX = CoreMatchers.equalTo(VIN_DESC_COLUMN_INDEX);
@@ -41,8 +40,11 @@ public class WorkbookBuilderIntegrationTest {
 	private static final String DESC_10C116 = "ЗАМЕНА КЛЕММ КАБЕЛЯ АКБ(TSB No. In English : HCE11-11-P180-RBMDENFDLMTQCMHRNF)";
 	private static final String DESC_10CR07 = "SOLARIS / ACCENT (RBr) ПЛАСТИКОВАЯ ШАЙБА ВЕДУЩЕГО ВАЛА (ОБЕ СТОРОНЫ) СНЯТИЕ";
 	private static final String DESC_10CR08 = "SOLARIS / ACCENT (RBr) ЗАМЕНА РЕЙКИ РУЛЕВОГО УПРАВЛЕНИЯ С УСИЛИТЕЛЕМ В СБОРЕ";
+    private static final String CAMPAIGN_NUMBER_TITLE = "Номер кампании";
+    private static final String CAMPAIGN_DESCRIPTION_TITLE = "Описание";
+    private static final int COLUMN_INDEX_DESCRIPTION_CLIENT2 = 6;
 
-	private ColumnReaderHelper columnReaderHelper;
+    private ColumnReaderHelper columnReaderHelper;
 	private ColumnExcelReader columnExcelReader;
 	private CampaignProcessor campaignProcessor;
 	private VinListProcessor vinListProcessor;
@@ -59,7 +61,7 @@ public class WorkbookBuilderIntegrationTest {
 	}
 
 	@Test
-	public void formLinkedDocument() throws IOException, InvalidFormatException {
+	public void addTasksToClientsWithoutOffset() throws IOException, InvalidFormatException {
 		final Workbook clientWB = WorkbookFactory.create(CLIENT_FILE);
 		final Sheet clientSheet = clientWB.getSheetAt(0);
 		final Cell[] bodyIds = columnExcelReader.getColumnValues(clientSheet, BODY_ID_MARKER);
@@ -69,14 +71,14 @@ public class WorkbookBuilderIntegrationTest {
 		final ListMultimap<String, Cell> linkedBodyIdWithCampaigns = campaignProcessor.linkBodyIdWithCampaigns(bodyIds,
 				CAMPAIGN_FILE, VIN_MARKER);
 		final Map<String, String> bodyIdDescriptionMap = vinListProcessor.mapVinListIdToDescription(CAMPAIGN_FILE,
-				"Номер кампании", "Описание");
+                CAMPAIGN_NUMBER_TITLE, CAMPAIGN_DESCRIPTION_TITLE);
 		excelBuilder.setVinListDescriptionMap(bodyIdDescriptionMap);
 		excelBuilder.assignTasks(bodyIds, linkedBodyIdWithCampaigns);
 		excelBuilder.saveToFile(LINKED_RESULT_PATH.toFile());
-		verifyResults();
+		verifyWithoutOffsetResults();
 	}
 
-	private void verifyResults() throws IOException, InvalidFormatException {
+	private void verifyWithoutOffsetResults() throws IOException, InvalidFormatException {
 		final Workbook workbookForVerification = WorkbookFactory.create(LINKED_RESULT_PATH.toFile());
 		final Sheet verifyClientSheet = workbookForVerification.getSheetAt(0);
 		final Iterator<Row> clientIterator = verifyClientSheet.rowIterator();
@@ -108,7 +110,30 @@ public class WorkbookBuilderIntegrationTest {
 				DESC_COL_INDEX);
 	}
 
-	@Test
+    @Test
+    public void addTasksToClientsWithOffset() throws Exception {
+        final Workbook clientWB = WorkbookFactory.create(CLIENT_FILE2);
+        final Sheet clientSheet = clientWB.getSheetAt(0);
+        final Cell[] bodyIds = columnExcelReader.getColumnValues(clientSheet, "VIN номер автомобиля");
+        final DataBuilder excelBuilder = new WorkbookBuilder();
+        excelBuilder.useWorkbook(clientWB);
+        excelBuilder.setPathToCampaignFile(CAMPAIGN_FILE2.getName());
+        final ListMultimap<String, Cell> linkedBodyIdWithCampaigns = campaignProcessor.linkBodyIdWithCampaigns(bodyIds,
+                CAMPAIGN_FILE2, VIN_MARKER);
+        final Map<String, String> bodyIdDescriptionMap = vinListProcessor.mapVinListIdToDescription(CAMPAIGN_FILE2,
+                CAMPAIGN_NUMBER_TITLE, CAMPAIGN_DESCRIPTION_TITLE);
+        excelBuilder.setVinListDescriptionMap(bodyIdDescriptionMap);
+        excelBuilder.assignTasks(bodyIds, linkedBodyIdWithCampaigns);
+        excelBuilder.saveToFile(LINKED_RESULT_PATH2.toFile());
+
+        final Workbook workbookForVerification = WorkbookFactory.create(LINKED_RESULT_PATH2.toFile());
+        final Sheet verifyClientSheet = workbookForVerification.getSheetAt(0);
+        final Iterator<Row> clientIterator = verifyClientSheet.rowIterator();
+        final Cell cell_KMHSH81XDBU763142_Desc = columnReaderHelper.findCell(clientIterator, "ЗАМЕНА КЛЕММ КАБЕЛЯ АКБ(TSB No. In English : HCE11-11-P180-RBMDENFDLMTQCMHRNF)");
+        assertEquals(COLUMN_INDEX_DESCRIPTION_CLIENT2, cell_KMHSH81XDBU763142_Desc.getColumnIndex());
+    }
+
+    @Test
 	@Ignore
 	public void printResultFile() throws IOException, InvalidFormatException {
 		final Workbook workbookForVerification = WorkbookFactory.create(LINKED_RESULT_PATH.toFile());
